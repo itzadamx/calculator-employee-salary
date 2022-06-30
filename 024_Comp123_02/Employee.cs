51cd882b35c8ee916bbba34473929208301f4e50
itzadamx <fcbpro1337@gmail.com>
2022-06-30 19:53:57 -0400

    Add project files.

---
+++ 024_Comp123_02/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024_Comp123_02
+{
+    class Employee
+    { 
+        private int employeeUserId;
+        private string firstName;
+        private string lastName;
+        private double baseSalary = 3000;
+        private double grossSales;
+        private double comissionRate = 0.3;
+
+        public int EmployeeUserId { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public double BaseSalary { get; set; }
+        public double GrossSales { get; set; }
+
+        public double ComissionRate { get; set; }
+
+        public Employee()
+        {
+            employeeUserId = EmployeeUserId;
+            firstName = FirstName;
+            lastName = LastName;
+            baseSalary = BaseSalary;
+            grossSales = GrossSales;
+            comissionRate = ComissionRate;
+        }
+        //constructor
+        public Employee(int employeeUserId, string firstName, double baseSalary)
+        {
+            employeeUserId = EmployeeUserId;
+            firstName = FirstName;
+            baseSalary = BaseSalary;
+        }
+        public double Earnings()
+        {
+            double commission = ComissionRate * GrossSales + BaseSalary;
+            return commission;
+        }
+
+    }
+}
---
+++ 024_Comp123_02/EmployeeTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024_Comp123_02
+{
+    class EmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            // Group 024 members: Adam Daabaz- Ignacio Agustin Zuluaga -Sohyeon Song
+
+            // Loop checkers for both Gross sales and commission rates.
+            bool validGrossSale = false;
+            bool validCommissionRate = false;
+
+            // Intro for the user
+            Console.WriteLine("---------* Employee Information *--------");
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------");
+
+            // First object
+            Employee employee1 = new Employee(301159644, "Ignacio", 4000);
+            
+            //Second object
+            Employee employee2 = new Employee();
+            Console.Write("Please insert your First Name:");
+            employee2.FirstName = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Please insert your Last name:");
+            employee2.LastName = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Please insert your base salary:");
+            employee2.BaseSalary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            // checking the input for Gross sales and Commission rate.
+            while (!validGrossSale)
+            {
+                try
+                {
+                    Console.Write("Please insert your gross sales:");
+                    employee2.GrossSales = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine();
+                    if (employee2.GrossSales <= 0)
+                    {
+                        throw new Exception(" ***INPUT-ERROR*** 'Gross sales cannot be negative or 0' ");
+                    }
+                    else
+                    {
+                        validGrossSale = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (!validCommissionRate)
+            {
+                try
+                {
+                    Console.Write("Please insert your commission rate:");
+                    employee2.ComissionRate = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine();
+                    if (employee2.ComissionRate <= 0)
+                    {
+                        Console.WriteLine();
+                        throw new Exception(" ***INPUT-ERROR*** 'Commission rate cannot be negative or 0' ");
+                    }
+                    else if (employee2.ComissionRate < 0.2 || employee2.ComissionRate > 1)
+                    {
+                        Console.WriteLine();
+                        throw new Exception(" ***INPUT-ERROR*** 'Commission rate should be in between 0.2 - 1' ");
+                    }
+                    else
+                    {
+                        validCommissionRate = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($" Hello Mr/Mrs {employee2.LastName}, your final Employee Earnings:{employee2.Earnings():C}");
+            Console.ReadKey();
+        }
+    }
+}
+
+ 
